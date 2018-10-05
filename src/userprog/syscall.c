4b7099714f80715ce900f3c116a4f8ba267fc17a
almyk <hansentommy92@gmail.com>
2018-10-05 01:43:55 -0700

    Start working on syscall_handler, invalid esp handling

--- src/userprog/syscall.c
+++ src/userprog/syscall.c
@@ -4,6 +4,8 @@
 #include "threads/interrupt.h"
 #include "threads/thread.h"
 
+#include "threads/vaddr.h"
+
 static void syscall_handler (struct intr_frame *);
 
 void
@@ -13,8 +15,24 @@ syscall_init (void)
 }
 
 static void
-syscall_handler (struct intr_frame *f UNUSED) 
+syscall_handler (struct intr_frame *f) 
 {
+  /* 3.3.4 System Calls code block */
+  void *esp = f->esp;
+  uint32_t *pd = thread_current ()->pagedir;
+  int sysnum = *(int*)esp;
+
+  if (!is_user_vaddr(esp) || esp == NULL)
+    {
+      pagedir_destroy (pd);
+      thread_exit ();
+    }
+  printf("\n--- syscall_handler ---\n\n");
+  printf("esp value: %x\n", esp);
+  printf("sysnum : %d\n", sysnum);
+  hex_dump(esp, esp, 0x80, 1);
+  /* end of 3.3.4 block */
+
   printf ("system call!\n");
   thread_exit ();
 }
